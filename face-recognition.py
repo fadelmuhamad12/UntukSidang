e735467a3d7a6e20db4df8bcce5d36195b8a2686
fadel muhamad <fadelmuhamad12.fm@gmail.com>
2023-06-26 00:47:53 +0700

    wajah sudah berhasil terdeteksi

--- face-recognition.py
+++ face-recognition.py
@@ -1,3 +1,5 @@
+from lzma import PRESET_DEFAULT
+from tkinter.font import names
 from turtle import width
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -29,6 +31,16 @@ class MainApp(QMainWindow, ui):
         self.ATTENDANCEBACK2.clicked.connect(self.tampilHalamanUtama)
         self.REPORTSBACK.clicked.connect(self.tampilHalamanUtama)
         self.TRAININGBUTTON.clicked.connect(self.start_training)
+        self.RECORD.clicked.connect(self.record_attendance)
+        try: 
+            con = sqlite3.connect("face-reco.db")
+            con.execute("CREATE TABLE IF NOT EXISTS attendance(attendanceid INTEGER, name Text, attendancedate TEXT)")
+            con.commit()
+            print("Tabel Berhasil Dibuat")
+        except:
+            print("Terdapat Error di Database")
+    
+
 
 
 
@@ -36,7 +48,7 @@ class MainApp(QMainWindow, ui):
     #-------LOGIN PROSES--------#
     def login(self):
         kataSandi = self.PASSWORD.text()
-        if (kataSandi == "Gunadarma2019"):
+        if (kataSandi == "1"):
             self.PASSWORD.setText("")
             self.LOGININFO.setText("")
             self.tabWidget.setCurrentIndex(1) #jika Passwordnya true maka akan ke halaman utama
@@ -54,6 +66,7 @@ class MainApp(QMainWindow, ui):
 
         #-------Absen Mahasiswa Prosses--------#
     def tampilHalamanUtama(self):
+        self.currentprocess.setText("Belum Melakukan Absensi")
         self.tabWidget.setCurrentIndex(1)
 
 
@@ -91,7 +104,7 @@ class MainApp(QMainWindow, ui):
     #         count = 1
     #         while count < int(self.trainingCount.text()) + 1:
     #             print(count)
-    #             (_,im) = webcam.read()
+    #             ret ,im = webcam.read()
     #             gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) #ini untuk convert image training menjadi warna greysacle
     #             faces = face_cascade.detectMultiScale(gray,1.3,4)
     #             for (x,y,w,h) in faces:
@@ -143,6 +156,67 @@ class MainApp(QMainWindow, ui):
             QMessageBox.information(self, "Training Wajah Berhasil", "Training wajah telah selesai.")
             self.traineeName.setText("")
             self.trainingCount.setText("100")
+
+    # Face Recognition #
+    def record_attendance(self):
+        self.currentprocess.setText("Proses Dimulai..Mohon Tunggu..")
+        haar_file = 'haarcascade_frontalface_default.xml'
+        face_cascade = cv2.CascadeClassifier(haar_file)
+        datasets = "datasets"
+        (images,labels,names,id) =([],[],{},0)
+        for(subdirs,dirs,files) in os.walk(datasets):
+            for subdir in dirs:
+                names[id] = subdir
+                subjectpath =os.path.join(datasets,subdir)
+                for filename in os.listdir(subjectpath):
+                    path = subjectpath + "/" + filename 
+                    label = id
+                    images.append(cv2.imread(path,0)) #ini untuk nambahin nama ke daftar absen
+                    labels.append(int(label)) #ini untuk nambahin id ke daftar absen
+                id += 1
+        (images,labels) = [numpy.array(lis) for lis in [images,labels]]
+        print(images,labels) 
+        (width, height) = (130,100)
+        model = cv2.face.LBPHFaceRecognizer_create() #membuat variabel model dari algoritma LBPH
+        model.train(images,labels)
+        webcam = cv2.VideoCapture(0)
+        cnt = 0
+        while True:
+            (_,im) = webcam.read()
+            gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray,1.3,5) #untuk mendeteksi semua muka yg ada di frame
+            for (x,y,w,h) in faces:
+                cv2.rectangle(im,(x,y),(x+w,y+h),(255,255,0),2)
+                face = gray[y:y+h,x:x+w]
+                face_resize = cv2.resize(face,(width,height))
+                confidence =  model.predict(face_resize) #ini untuk level of confidence
+                cv2.rectangle(im,(x,y),(x+w, y+h),(0,255,0),3)
+                if(confidence[1]<800):
+                    cv2.putText(im,'%s-%.0f'%(names[confidence[0]],confidence[1]),(x-10,y-10),cv2.FONT_HERSHEY_PLAIN,2,(0,0,255),2)
+                    print(names[confidence[0]])
+                    self.currentprocess.setText("Wajah Terdaftar" + names[confidence[0]])
+                else:
+                    cnt+=1
+                    cv2.putText(im,"Tidak Dikenal",(x-10,y-10),cv2.FONT_HERSHEY_PLAIN,2,(0,255,0),2)
+                    if(cnt>100):
+                        print("Wajah Tidak Dikenal")
+                        self.currentprocess.setText("Wajah Tidak Dikenal" + names[confidence[0]])
+                        cv2.imwrite("unKnown.jpg",im)
+                        cnt=0
+            cv2.imshow("Face Recognition",im)
+            key = cv2.waitKey(10)
+            if key==27:
+                break
+        webcam.release()
+        cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
 
 
 
